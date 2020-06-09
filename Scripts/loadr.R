f6bb796a414afa4750c9e192063d7af63d423f2a
Luis Carlos Quesada <luiscarlosquesada@gmail.com>
2020-06-09 16:23:23 -0600

    loadr

---
+++ Scripts/loadr.R
@@ -0,0 +1,20 @@
+library(readr)
+
+data_mpg <- read_csv("Dataset/gas_mileage_comsumption_mpg.csv")
+
+cols(
+    X1 = col_double(),
+    manufacturer = col_character(),
+    model = col_character(),
+    displ = col_double(),
+    year = col_double(),
+    cyl = col_double(),
+    trans = col_character(),
+    drv = col_character(),
+    cty = col_double(),
+    hwy = col_double(),
+    fl = col_character(),
+    class = col_character()
+  )
+
+View(data_mpg)
