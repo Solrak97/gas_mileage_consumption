8840b3f40a703a7214e0393e25646a20f9850056
Luis Carlos Quesada <luiscarlosquesada@gmail.com>
2020-06-12 16:36:33 -0600

    packages

--- Scripts/loadr.R
+++ Scripts/loadr.R
@@ -1,24 +1,25 @@
-loadr <- function(){
-  
-  library(readr)
+library(readr)
 
-  data_mpg <- read_csv("Dataset/gas_mileage_comsumption_mpg.csv")
 
-  cols(
-      X1 = col_double(),
-      manufacturer = col_character(),
-      model = col_character(),
-      displ = col_double(),
-      year = col_double(),
-      cyl = col_double(),
-      trans = col_character(),
-      drv = col_character(),
-      cty = col_double(),
-      hwy = col_double(),
-      fl = col_character(),
-      class = col_character()
-    )
-}
+
+data_mpg <- read_csv("Dataset/gas_mileage_comsumption_mpg.csv")
+
+cols(
+  X1 = col_double(),
+  manufacturer = col_character(),
+  model = col_character(),
+  displ = col_double(),
+  year = col_double(),
+  cyl = col_double(),
+  trans = col_character(),
+  drv = col_character(),
+  cty = col_double(),
+  hwy = col_double(),
+  fl = col_character(),
+  class = col_character()
+)
+
+
 
 library(tidyverse)
 ano99= filter(data_mpg, year=="1999")
---
+++ Scripts/packages.R
@@ -0,0 +1,3 @@
+install.packages("tidyverse")
+install.package("readr")
+#install.packages()
