6d33ca6097579c96d8dcb1fa52daf76643b869e5
Luis Carlos Quesada <luiscarlosquesada@gmail.com>
2020-07-08 21:08:57 -0600

    final data

--- Scripts/loadr.R
+++ Scripts/loadr.R
@@ -83,3 +83,85 @@ mean(transM99$cty)
 mean(transAuto99$hwy)
 mean(transM08$cty)
 mean(transAuto08$hwy)
+
+
+
+
+#Pruebas de relacion - significancia displ <=> consumo
+
+#Prueba de asociacion dspl - hwy
+chisq.test(data_mpg$displ, data_mpg$hwy, simulate.p.value = TRUE)
+
+#Prueba de asociacion dspl - cty
+chisq.test(data_mpg$displ, data_mpg$cty, simulate.p.value = TRUE)
+
+#Valores de chi cuadrado significativos, variables fuertemente relacionadas
+
+
+
+
+#Pruebas de normalidad
+with(data_mpg, shapiro.test(data_mpg$displ))
+with(data_mpg, qqPlot(data_mpg$displ))
+
+#Normal para diferentes niveles de dspl
+with(motoresS, shapiro.test(motoresS$displ))
+with(motoresS, qqPlot(motoresS$displ))
+
+with(motoresM, shapiro.test(motoresM$displ))
+with(motoresM, qqPlot(motoresM$displ))
+
+with(motoresXL, shapiro.test(motoresXL$displ))
+with(motoresXL, qqPlot(motoresXL$displ))
+
+#Datos son normales, en el caso de motores XL apenas normales
+
+
+
+#T tests
+#motores S
+t.test(motoresS$cty, data_mpg$cty, var.equal = TRUE)
+t.test(motoresS$hwy, data_mpg$hwy, var.equal = TRUE)
+
+#Motor consume muy poco en gran cantidad de millas
+
+
+#motores M
+t.test(motoresM$cty, data_mpg$cty, var.equal = TRUE)
+t.test(motoresM$hwy, data_mpg$hwy, var.equal = TRUE)
+
+#motores medianos son similares a la media en ciudad, ligeramente menos eficientes en carretera
+
+
+#motores XL
+t.test(motoresXL$cty, data_mpg$cty, var.equal = TRUE)
+t.test(motoresXL$hwy, data_mpg$hwy, var.equal = TRUE)
+
+#Los motores grandes tienen alto consumo en ciudad y carretera 
+
+
+
+#comparacion datos M - XL
+t.test(motoresM$cty, motoresXL$cty, var.equal = TRUE)
+t.test(motoresM$hwy, motoresXL$hwy, var.equal = TRUE)
+
+#El consumo de motores medianos es significativamente menor a los XL
+#Principalmente en carretera
+
+
+#comparacion datos S - XL
+t.test(motoresS$cty, motoresXL$cty, var.equal = TRUE)
+t.test(motoresS$hwy, motoresXL$hwy, var.equal = TRUE)
+
+#El consumo de motores medianos es significativamente menor a los XL
+#Principalmente en carretera
+
+
+#comparacion datos S - M
+t.test(motoresS$cty, motoresM$cty, var.equal = TRUE)
+t.test(motoresS$hwy, motoresM$hwy, var.equal = TRUE)
+
+#El consumo de motores medianos es significativamente menor a los XL
+#Principalmente en carretera
+
+#Por lo tanto se cumple la hipotesis de que los autos de menor dspl consumen menos combustible
